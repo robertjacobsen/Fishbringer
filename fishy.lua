61f3d40332768d333df3752f7390a3dca947dad8
Robert Jacobsen <robert@robertjacobsen.net>
2019-09-07 13:16:22 +0200

    Adapt Fishbringer to work with Classic

--- fishy.lua
+++ fishy.lua
@@ -1,11 +1,11 @@
+FishbringerDB = {}
 local FONT = select(1, GameFontNormalSmall:GetFont())
 local fb = CreateFrame"Frame"
 local db
-
 local char = string.format("%s - %s", UnitName"player", GetRealmName())
 
-fb:RegisterEvent"ADDON_LOADED"
-fb:RegisterEvent"CHAT_MSG_SPELL_ITEM_ENCHANTMENTS"
+fb:RegisterEvent"PLAYER_ENTERING_WORLD"
+fb:RegisterEvent"COMBAT_LOG_EVENT"
 fb:RegisterEvent"SKILL_LINES_CHANGED"
 fb:RegisterEvent"LOOT_OPENED"
 fb:RegisterEvent"ZONE_CHANGED_NEW_AREA"
@@ -13,7 +13,7 @@ fb:RegisterEvent"ZONE_CHANGED"
 fb:RegisterEvent"UNIT_INVENTORY_CHANGED"
 
 local function Print(text)
-	ChatFrame1:AddMessage(string.format("|cff33ff99Fishbringer|r: %s", text))
+	ChatFrame1:AddMessage(string.format("|cffff8000Fishbringer|r: %s", text))
 end
 
 local zones = {
@@ -104,238 +104,354 @@ local subzones = {
 	["Lake Jorune"] = 405,
 }
 
-local fishingpoles = { [6256] = true, [6365] = true, [6366] = true, [6367] = true, [12225] = true, [19022] = true, [19970] = true, [25978] = true, [44050] = true, }
+local fishingpoles = { 
+	[6256] = true,
+	[6365] = true,
+	[6366] = true,
+	[6367] = true,
+	[12225] = true,
+	[19022] = true,
+	[19970] = true,
+	[25978] = true,
+	[44050] = true,
+}
 
-local fishingSkill, fishingBuff, fishCaught, fishToCatch = 0
-
-local function UpdateDatabase()
-	db[char].fishingSkill = fishingSkill
-	db[char].fishingBuff = fishingBuff
-	db[char].fishCaught = fishCaught
-	db[char].fishToCatch = fishToCatch
-end
-
-local function FishyFormula(r)
-	if r <= 75 then
+local function GetNumFishToLevel(skillRank)
+	if skillRank <= 75 then
 		return 1
 	else
-		return math.ceil((r - 75) / 25)
+		return math.ceil((skillRank - 75) / 25)
 	end
 end	
 
-Fishbringer = CreateFrame("Frame", "Fishbringer", UIParent)
-Fishbringer:EnableMouse(true)
-Fishbringer:SetMovable(true)
-Fishbringer:SetUserPlaced(true)
-Fishbringer:SetHeight(150)
-Fishbringer:SetWidth(185)
-Fishbringer:SetBackdrop({bgFile = "Interface\\ChatFrame\\ChatFrameBackground", tile = true, tileSize = 16, insets = {left = 4, right = 4, top = 4, bottom = 4},})
-Fishbringer:SetBackdropColor(0, 0, 0, 0)
-Fishbringer:SetScript("OnMouseDown", function(self) if(IsAltKeyDown()) then self:SetBackdropColor(0, 0, 0, .6) self:StartMoving() end end)
-Fishbringer:SetScript("OnMouseUp", function(self) self:StopMovingOrSizing() self:SetBackdropColor(0, 0, 0, 0) end)
-Fishbringer:SetPoint("CENTER")
-Fishbringer:Hide()
-
-local title = Fishbringer:CreateFontString(nil, "OVERLAY")
-title:SetHeight(15)
-title:SetWidth(Fishbringer:GetWidth()-10)
-title:SetPoint("TOPLEFT", Fishbringer, "TOPLEFT", 3, -10)
-title:SetJustifyH("CENTER")
-title:SetFont(FONT, 16)
-title:SetShadowOffset(1, -1)
-local name
-if random(100) < 10 then 
-	name = "Corrupted Fishbringer"
-else
-	name = "Fishbringer"
-end
-title:SetText(name)
-Fishbringer.title = title
-
-local zone = Fishbringer:CreateFontString(nil, "OVERLAY")
-zone:SetHeight(50)
-zone:SetWidth(Fishbringer:GetWidth()-10)
-zone:SetPoint("TOP", Fishbringer.title, "BOTTOM", 0, 0)
-zone:SetJustifyH("CENTER")
-zone:SetFont(FONT, 12)
-zone:SetShadowOffset(1, -1)
-Fishbringer.zone = zone
-
-local catch = Fishbringer:CreateFontString(nil, "OVERLAY")
-catch:SetHeight(15)
-catch:SetWidth(Fishbringer:GetWidth()-10)
-catch:SetPoint("TOP", Fishbringer.zone, "BOTTOM", 0, 0)
-catch:SetJustifyH("CENTER")
-catch:SetFont(FONT, 14)
-catch:SetShadowOffset(1, -1)
-Fishbringer.catch = catch
-
-local content = Fishbringer:CreateFontString(nil, "OVERLAY")
-content:SetHeight(30)
-content:SetWidth(Fishbringer:GetWidth()-10)
-content:SetFont(FONT, 12)
-content:SetShadowOffset(1, -1)
-content:SetJustifyH("CENTER")
-content:SetPoint("TOP", Fishbringer.catch, "BOTTOM", 0, 0)
-Fishbringer.content = content
-
-local fish = Fishbringer:CreateFontString(nil, "OVERLAY")
-fish:SetHeight(15)
-fish:SetWidth(Fishbringer:GetWidth()-10)
-fish:SetFont(FONT, 12)
-fish:SetShadowOffset(1, -1)
-fish:SetJustifyH("CENTER")
-fish:SetPoint("TOP", Fishbringer.content, "BOTTOM", 0, -7)
-Fishbringer.fish = fish
-
-local function UpdateFishes(skipLoot)
-	if( IsFishingLoot() and not skipLoot) then
-		fishCaught = fishCaught + 1
+local function UpdateFishCount(shouldIncrement)
+	if shouldIncrement and IsFishingLoot() then
+		db[char].fishCaught = db[char].fishCaught + 1
 	end
-	Fishbringer.fish:SetFormattedText("%d of %d fish caught", fishCaught, fishToCatch)
-	UpdateDatabase()
+	Fishbringer.fishCount:SetFormattedText(
+		"%d of %d fish caught",
+		db[char].fishCaught,
+		db[char].fishToCatch
+	)
 end
 
-local function Fishes(f)
-	fishToCatch = f
-	fishCaught = 0
- 	UpdateFishes(true)
+local function IncrementFishCount()
+	UpdateFishCount(true)
 end
 
-local function UpdateZone()
-	local t
-	local z
-	t = GetSubZoneText()
-	z = subzones[t]
-	if not z then 
-		t = GetRealZoneText()
-		z = zones[t]
-	end
-	if not z then
-		z = 0
+local function ResetFishCounter(numFish)
+	db[char].fishToCatch = numFish
+	db[char].fishCaught = 0
+ 	UpdateFishCount(false)
+end
+
+local function UpdateCatchInfo()
+	local zoneText = GetSubZoneText()
+	local zoneSkill = subzones[zoneText]
+	local maxZoneSkill
+
+	if not zoneSkill then 
+		zoneText = GetRealZoneText()
+		zoneSkill = zones[zoneText]
+	
+		if not zoneSkill then
+			zoneSkill = 0
+			maxZoneSkill = 0
+		else
+			maxZoneSkill = zoneSkill + 95
+		end
 	end
 
-
-	local max, chance
-	max = z + 95
-	if max == 95 then max = 0 end
-	chance = ((fishingSkill + fishingBuff) - z)*0.01 + 0.05
-	if z < 0 then z = 1 end
+	local chance = (
+		(
+			db[char].fishingSkill + db[char].fishingBuff
+		) - zoneSkill
+	) * 0.01 + 0.05
+	
+	if zoneSkill < 0 then 
+		zoneSkill = 1
+	end
+	
 	if chance > 1 then 
 		chance = 1
 	elseif chance < 0 then 
 		chance = 0
 	end
-	local colour
-	if chance == 0 then
-		colour = "ffff2020"
-	elseif chance <= 0.5 then 
-		colour = "ffff8040"
-	elseif chance < 1 then 
-		colour = "ffffff00"
-	else
-		colour = "ff40bf40"
-	end
-	Fishbringer.zone:SetFormattedText("\124c%s%s\124r\n%d skill needed to fish\n(%d needed for 100%% catch rate)", colour, t, z, max, chance*100)
-	Fishbringer.catch:SetFormattedText("%d%% catch rate", chance*100)
-end
 
-local function GetSkill()
-	local _, _, _, fishing, _, _ = GetProfessions()
-	if (fishing) then
-		local name, _, rank, max, _, _, _, mod = GetProfessionInfo(fishing)
-		return name, rank, max, mod
+	local color
+	if chance == 0 then
+		color = "ffff2020"
+	elseif chance <= 0.5 then 
+		color = "ffff8040"
+	elseif chance < 1 then 
+		color = "ffffff00"
+	else
+		color = "ff40bf40"
 	end
-	return 0, 0, 0, 0
+	Fishbringer.zoneInfo:SetFormattedText(
+		"\124c%s%s\124r\n%d skill needed to fish\n(%d needed for 100%% catch rate)",
+		color, zoneText, zoneSkill, maxZoneSkill, chance * 100
+	)
+	Fishbringer.catchRate:SetFormattedText("%d%% catch rate", chance * 100)
 end
 
 local function UpdateSkill()
-	local skillName, skillRank, skillModifier, skillMaxRank
-
-	skillName, skillRank, skillMaxRank, skillModifier = GetSkill()
-
-	if skillName then
-		local fishNeeded = FishyFormula(skillRank)
-		if skillRank ~= skillMaxRank then 
-			Fishbringer.content:SetFormattedText("%d(+%d)/%d fishing skill\n%d fish needed at this level", skillRank, skillModifier, skillMaxRank, fishNeeded)
-		else
-			Fishbringer.content:SetFormattedText("%d(+%d) fishing skill", skillRank, skillModifier)
-		end
-		if fishingSkill ~= skillRank then
-			fishingSkill = skillRank
-			Fishes(fishNeeded)
-		end
-		if fishingBuff ~= skillModifier then
-			fishingBuff = skillModifier
-			UpdateZone()
-		end
-		UpdateDatabase()
+	if not db then 
+		return
 	end
+	local skillName, skillRank, skillModifier, skillMaxRank
+	local found = false
+	for i = 1, GetNumSkillLines() do
+		skillName, _, _, skillRank, _, skillModifier, skillMaxRank, _, _, _, _, _, _= GetSkillLineInfo(i)
+		if skillName == "Fishing" then 
+			found = true
+			break
+		end
+	end
+	if found then
+		local fishNeeded = GetNumFishToLevel(skillRank)
+		if skillRank ~= skillMaxRank then 
+			local skillModifierText = ""
+			if skillModifier > 0 then
+				skillModifierText = string.format("(+%d)", skillModifier)
+			end
+			Fishbringer.content:SetFormattedText(
+				"%d%s/%d fishing skill\n%d fish needed at this level",
+				skillRank,
+				skillModifierText,
+				skillMaxRank,
+				fishNeeded
+			)
+		else
+			local skillModifierText = ""
+			if skillModifier > 0 then
+				skillModifierText = string.format("(+%d)", skillModifier)
+			end
+			Fishbringer.content:SetFormattedText(
+				"%d%s fishing skill",
+				skillRank,
+				skillModifierText
+			)
+		end
+		if db[char].fishingSkill ~= skillRank then
+			db[char].fishingSkill = skillRank
+			ResetFishCounter(fishNeeded)
+		end
+		if db[char].fishingBuff ~= skillModifier then
+			db[char].fishingBuff = skillModifier
+			UpdateCatchInfo()
+		end
+	end
+end
+
+local function Update()
+	UpdateSkill()
+	UpdateCatchInfo()
+end
+
+local function CycleAlignment()
+	if db[char].alignment == "RIGHT" then
+		db[char].alignment = "LEFT"
+	elseif db[char].alignment == "LEFT" then
+		db[char].alignment = "CENTER"
+	else
+		db[char].alignment = "RIGHT"
+	end
+
+	-- Update all
+	Fishbringer.title:SetJustifyH(db[char].alignment)
+	Fishbringer.zoneInfo:SetJustifyH(db[char].alignment)
+	Fishbringer.catchRate:SetJustifyH(db[char].alignment)
+	Fishbringer.content:SetJustifyH(db[char].alignment)
+	Fishbringer.fishCount:SetJustifyH(db[char].alignment)
 end
 
 local function Toggle()
-	if Fishbringer:IsShown() == 1 then 
+	if db[char].isShown then 
 		Fishbringer:Hide()
+		db[char].isShown = false
 	else
-		UpdateSkill()
-		UpdateZone()
+		Update()
 		Fishbringer:Show()
-	end
-end 
-
-local function CheckPole() 
-	local _,_,itemid = string.find(GetInventoryItemLink("player", GetInventorySlotInfo("MainHandSlot")) or "", "item:(%d+):(.+)")
-	if fishingpoles[tonumber(itemid)] then
-		UpdateSkill()
-		UpdateZone()
-		Fishbringer:Show()
-	else
-		Fishbringer:Hide()
+		db[char].isShown = true
 	end
 end
 
-fb:SetScript("OnEvent", function(self, event, ...) 
+local function ToggleFishCount()
+	if db[char].isFishCountShown then
+		Fishbringer.fishCount:Hide()
+		db[char].isFishCountShown = false
+	else
+		Fishbringer.fishCount:Show()
+		db[char].isFishCountShown = true
+	end
+end
+
+local function CheckForFishingPole() 
+
+	local _, _, itemid = string.find(GetInventoryItemLink("player", GetInventorySlotInfo("MainHandSlot")) or "", "item:(%d+):(.+)")
+	if db[char].isShown or fishingpoles[tonumber(itemid)] then
+		Update()
+		Fishbringer:Show()
+		db[char].isShown = true
+	else
+		Fishbringer:Hide()
+		db[char].isShown = false
+	end
+end
+
+local function InitializeDB()
+	db = FishbringerDB
+	if not db then 
+		db = {}
+		FishbringerDB = db
+	end
+
+	if not db[char] then 
+		db[char] = {
+			fishingSkill = 0, 
+			fishingBuff = 0,
+			fishCaught = 0,
+			fishToCatch = 0,
+			isShown = false,
+			isFishCountShown = true,
+			alignment = "RIGHT"
+		}
+	end
+end
+
+local function InitializeFrame()
+	-- Frame madness
+	Fishbringer = CreateFrame("Frame", "Fishbringer", UIParent)
+	Fishbringer:EnableMouse(true)
+	Fishbringer:SetMovable(true)
+	Fishbringer:SetUserPlaced(false)
+	Fishbringer:SetHeight(150)
+	Fishbringer:SetWidth(185)
+	Fishbringer:SetBackdrop({
+		bgFile = "Interface\\ChatFrame\\ChatFrameBackground", 
+		tile = true, 
+		tileSize = 16, 
+		insets = {
+			left = 4, 
+			right = 4, 
+			top = 4,
+			bottom = 4
+		},
+	})
+	Fishbringer:SetBackdropColor(0, 0, 0, 0)
+	Fishbringer:SetScript("OnMouseDown", function(self) 
+		if IsAltKeyDown() then 
+			self:SetBackdropColor(0, 0, 0, .6)
+			self:StartMoving() 
+		end 
+	end)
+	Fishbringer:SetScript("OnMouseUp", function(self) 
+		self:StopMovingOrSizing()
+		self:SetBackdropColor(0, 0, 0, 0)
+	end)
+	Fishbringer:SetPoint("RIGHT", UIParent, "RIGHT", -10, 0)
+
+	if not db[char].isShown then
+		Fishbringer:Hide()
+	end
+
+	local title = Fishbringer:CreateFontString(nil, "OVERLAY")
+	title:SetHeight(15)
+	title:SetWidth(Fishbringer:GetWidth() - 10)
+	title:SetPoint("TOPLEFT", Fishbringer, "TOPLEFT", 3, -10)
+	title:SetJustifyH(db[char].alignment)
+	title:SetFont(FONT, 16)
+	title:SetShadowOffset(1, -1)
+	local name
+	if random(100) < 5 then 
+		name = "Corrupted Fishbringer"
+	else
+		name = "Fishbringer"
+	end
+	title:SetText(name)
+	Fishbringer.title = title
+
+	local zoneInfo = Fishbringer:CreateFontString(nil, "OVERLAY")
+	zoneInfo:SetHeight(50)
+	zoneInfo:SetWidth(Fishbringer:GetWidth() - 10)
+	zoneInfo:SetPoint("TOP", Fishbringer.title, "BOTTOM", 0, 0)
+	zoneInfo:SetJustifyH(db[char].alignment)
+	zoneInfo:SetFont(FONT, 12)
+	zoneInfo:SetShadowOffset(1, -1)
+	Fishbringer.zoneInfo = zoneInfo
+
+	local catchRate = Fishbringer:CreateFontString(nil, "OVERLAY")
+	catchRate:SetHeight(15)
+	catchRate:SetWidth(Fishbringer:GetWidth() - 10)
+	catchRate:SetPoint("TOP", Fishbringer.zoneInfo, "BOTTOM", 0, 0)
+	catchRate:SetJustifyH(db[char].alignment)
+	catchRate:SetFont(FONT, 14)
+	catchRate:SetShadowOffset(1, -1)
+	Fishbringer.catchRate = catchRate
+
+	local content = Fishbringer:CreateFontString(nil, "OVERLAY")
+	content:SetHeight(30)
+	content:SetWidth(Fishbringer:GetWidth() - 10)
+	content:SetFont(FONT, 12)
+	content:SetShadowOffset(1, -1)
+	content:SetJustifyH(db[char].alignment)
+	content:SetPoint("TOP", Fishbringer.catchRate, "BOTTOM", 0, 0)
+	Fishbringer.content = content
+
+	local fishCount = Fishbringer:CreateFontString(nil, "OVERLAY")
+	fishCount:SetHeight(15)
+	fishCount:SetWidth(Fishbringer:GetWidth() - 10)
+	fishCount:SetFont(FONT, 12)
+	fishCount:SetShadowOffset(1, -1)
+	fishCount:SetJustifyH(db[char].alignment)
+	fishCount:SetPoint("TOP", Fishbringer.content, "BOTTOM", 0, -7)
+	Fishbringer.fishCount = fishCount
+
+	if not db[char].isFishCountShown then
+		Fishbringer.fishCount:Hide()
+	end
+end
+
+fb:SetScript("OnEvent", function(self, event, ...)
 	self[event](self, event, ...)
 end)
 
-fb.ADDON_LOADED = function(self, event, addon) 
-	if addon == 'Fishbringer' then
-		db = FishbringerDB
-		if not db then 
-			db = {}
-			FishbringerDB = db
-		end
-
-		if not db[char] then 
-			db[char] = {
-				fishingSkill = 0, 
-				fishingBuff = 0,
-				fishCaught = 0,
-				fishToCatch = 0
-			}
-		end
-	
-		fishingSkill = db[char].fishingSkill
-		fishingBuff = db[char].fishingBuff
-		fishCaught = db[char].fishCaught
-		fishToCatch = db[char].fishToCatch
-
-		CheckPole()
-	end
+fb.PLAYER_ENTERING_WORLD = function()
+	InitializeDB()
+	InitializeFrame()
+	CheckForFishingPole()
+	UpdateFishCount(false)
 end
 
-fb.CHAT_MSG_SPELL_ITEM_ENCHANTMENTS = UpdateSkill
+fb.COMBAT_LOG_EVENT = UpdateSkill
 fb.SKILL_LINES_CHANGED = UpdateSkill
-fb.LOOT_OPENED = UpdateFishes
-fb.ZONE_CHANGED_NEW_AREA = UpdateZone
-fb.ZONE_CHANGED = UpdateZone
-fb.UNIT_INVENTORY_CHANGED = CheckPole
+fb.LOOT_OPENED = IncrementFishCount
+fb.ZONE_CHANGED_NEW_AREA = UpdateCatchInfo
+fb.ZONE_CHANGED = UpdateCatchInfo
+fb.UNIT_INVENTORY_CHANGED = CheckForFishingPole
 
-SlashCmdList["FISHBRINGER"] = function(arg1)
-	Toggle()
+local function ShowHelp()
+	Print("Nat Pagle would be proud of you.")
+	Print("Local variants:")
+	Print("- /fb show - Toggles visibility.")
+	Print("- /fb align - Cycles through text alignment.")
+	Print("- /fb count - Toggles fish count visibility.")
+end
+
+SlashCmdList["FISHBRINGER"] = function(arg)
+	if arg == "align" then
+		return CycleAlignment()
+	elseif arg == "show" or arg == "hide" then
+		return Toggle()
+	elseif arg == "count" then
+		return ToggleFishCount()
+	end
+	return ShowHelp()
 end
 SLASH_FISHBRINGER1 = "/fishbringer"
-SLASH_FISHBRINGER2 = "/fbr"
-SLASH_FISHBRINGER3 = "/fb"
-
+-- Hail to Fishing Buddy!
+if not select(4, GetAddOnInfo"Fishing Buddy") then
+	SLASH_FISHBRINGER2 = "/fb"
+end
 Print("Pack yer bags, we be leavin' fer fishin'!")
